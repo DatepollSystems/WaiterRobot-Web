69fe9d043171db8c23f998208bec617e10a90c9f
Dominik <git@dafnik.me>
2022-03-05 13:43:29 +0100

    [feat] Add delete all sessions

--- src/app/home/user-settings/sessions/sessions.component.ts
+++ src/app/home/user-settings/sessions/sessions.component.ts
@@ -5,6 +5,7 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {AbstractModelsListComponent} from '../../../_helper/abstract-models-list.component';
 import {SessionsService} from '../../../_services/models/user/sessions.service';
 import {SessionModel} from '../../../_models/user/session.model';
+import {QuestionDialogComponent} from '../../../_shared/question-dialog/question-dialog.component';
 
 @Component({
   selector: 'app-sessions',
@@ -16,5 +17,18 @@ export class SessionsComponent extends AbstractModelsListComponent<SessionModel>
 
   constructor(sessionsService: SessionsService, modal: NgbModal) {
     super(modal, sessionsService);
+  }
+
+  deleteAll(): void {
+    const modalRef = this.modal.open(QuestionDialogComponent, {ariaLabelledBy: 'modal-question-title', size: 'lg'});
+    modalRef.componentInstance.title = 'DELETE_CONFIRMATION';
+    void modalRef.result.then((result) => {
+      this.lumber.info('onDelete', 'Question dialog result:', result);
+      if (result?.toString().includes(QuestionDialogComponent.YES_VALUE)) {
+        for (const session of this.entities) {
+          this.entitiesService.delete(session.id);
+        }
+      }
+    });
   }
 }
