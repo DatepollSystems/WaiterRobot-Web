6a128275c1fae1a66e935574d3e432852a0adbf4
Dominik <git@dafnik.me>
2022-08-18 16:22:27 +0200

    add ping

--- src/app/home/start/start.component.ts
+++ src/app/home/start/start.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnDestroy} from '@angular/core';
 import {EnvironmentHelper} from '../../_helper/EnvironmentHelper';
 
 @Component({
@@ -6,10 +7,26 @@ import {EnvironmentHelper} from '../../_helper/EnvironmentHelper';
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss'],
 })
-export class StartComponent {
+export class StartComponent implements OnDestroy {
   isProduction = true;
 
-  constructor() {
+  responseFetchInterval?: number;
+  responseTime?: number;
+
+  constructor(httpClient: HttpClient) {
     this.isProduction = EnvironmentHelper.getProduction();
+
+    this.responseFetchInterval = setInterval(() => {
+      const startMs = new Date().getTime();
+      httpClient.get('/user/myself').subscribe(() => {
+        this.responseTime = new Date().getTime() - startMs;
+      });
+    }, 1000 * 2);
+  }
+
+  ngOnDestroy() {
+    if (this.responseFetchInterval) {
+      clearInterval(this.responseFetchInterval);
+    }
   }
 }
