bcdf7115f54f4210f4401ba57bb5e4bc692d9f2f
Dafnik <git@dafnik.me>
2022-09-27 18:46:31 +0200

    Try to fix returnUrl
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/_services/auth/auth-guard.service.ts
+++ src/app/_services/auth/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from './auth.service';
+import {HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
-      void this.router.navigate(['/about', {queryParams: {returnUrl: state.url}}]);
+      void this.router.navigate(['/about', {queryParams: new HttpParams().set('returnUrl', state.url)}]);
       return false;
     }
   }
