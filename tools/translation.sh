899a709f4896c7d6c298de241ae5ca4d0a0b0f61
Dafnik <git@dafnik.me>
2022-10-08 16:10:57 +0200

    Update dfx-translate with cli
    Signed-off-by: Dafnik <git@dafnik.me>

--- tools/translation.sh
+++ tools/translation.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 main () {
- node ./tools/generateTranslation.js https://translate.dafnik.me/translate de en
- node ./tools/generateTranslation.js https://translate.dafnik.me/translate de fr
- node ./tools/generateTranslation.js https://translate.dafnik.me/translate de es
- node ./tools/generateTranslation.js https://translate.dafnik.me/translate de it
- node ./tools/generateTranslation.js https://translate.dafnik.me/translate de pt
+ translate-cli autoTranslate https://translate.dafnik.me/translate en
+ translate-cli autoTranslate https://translate.dafnik.me/translate fr
+ translate-cli autoTranslate https://translate.dafnik.me/translate es
+ translate-cli autoTranslate https://translate.dafnik.me/translate it
+ translate-cli autoTranslate https://translate.dafnik.me/translate pt
 
  prettier --config ./.prettierrc --write ./src/assets/i18n
 
