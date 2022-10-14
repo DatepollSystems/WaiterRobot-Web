81d5bc286ebb8ff8ffe98fe0a6d75afcca9b3a7d
Dominik <git@dafnik.me>
2022-10-14 18:03:16 +0200

    remove return url

--- src/app/_services/auth/auth-guard.service.ts
+++ src/app/_services/auth/auth-guard.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from './auth.service';
-import {HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +12,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
-      void this.router.navigate(['/about', {queryParams: new HttpParams().set('returnUrl', state.url)}]);
+      void this.router.navigate(['/about']);
       return false;
     }
   }
