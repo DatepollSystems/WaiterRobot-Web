599919de5b024542ed4da96101e8ec828aa7e434
Dominik <git@dafnik.me>
2022-10-17 13:49:37 +0200

    ref
    Signed-off-by: Dominik <git@dafnik.me>

--- src/app/_shared/question-dialog/question-dialog.component.ts
+++ src/app/_shared/question-dialog/question-dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-question-modal',
