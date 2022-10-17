f7694dfdc7c75b62bc717d0859677edfff0aa238
Dominik <git@dafnik.me>
2022-10-17 19:57:08 +0200

    Align footer
    Signed-off-by: Dominik <git@dafnik.me>

--- src/app/_shared/footer/footer.module.ts
+++ src/app/_shared/footer/footer.module.ts
@@ -2,7 +2,6 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {DfxTranslateModule} from 'dfx-translate';
@@ -12,7 +11,7 @@ import {AboutModalComponent} from './modals/about-modal/about-modal.component';
 
 @NgModule({
   declarations: [FooterComponent, AboutModalComponent],
-  imports: [CommonModule, RouterModule, FormsModule, FlexLayoutModule, DfxTranslateModule, NgbNavModule],
+  imports: [CommonModule, RouterModule, FormsModule, DfxTranslateModule, NgbNavModule],
   exports: [FooterComponent],
 })
 export class FooterModule {}
