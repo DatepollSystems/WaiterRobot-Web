325b44377761a4c883d8ad404a6823fd33ff19b2
Dafnik <git@dafnik.me>
2022-11-05 12:34:00 +0100

    Make app-btn-toolbar scrollable
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/_shared/ui/app-btn-toolbar.component.ts
+++ src/app/_shared/ui/app-btn-toolbar.component.ts
@@ -1,15 +1,82 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   template: `
     <div class="pb-3 pt-1">
-      <div class="d-flex flex-wrap gap-3 gap-md-3">
+      <div
+        #contentWrapper
+        id="overflow-container"
+        class="d-flex flex-row flex-fill justify-content-sm-start overflow-auto user-select-none gap-3 gap-md-3">
         <ng-content></ng-content>
       </div>
     </div>
   `,
+  styles: [
+    `
+      #overflow-container {
+        scroll-snap-type: y mandatory;
+        scroll-behavior: smooth;
+      }
+
+      .overflow-auto::-webkit-scrollbar {
+        display: none;
+      }
+
+      /* Hide scrollbar for IE, Edge and Firefox */
+      .overflow-auto {
+        -ms-overflow-style: none; /* IE and Edge */
+        scrollbar-width: none; /* Firefox */
+      }
+
+      :host(btn-toolbar)::ng-deep.btn {
+        word-break: keep-all;
+        white-space: nowrap;
+      }
+    `,
+  ],
   selector: 'btn-toolbar',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppBtnToolbarComponent {}
+export class AppBtnToolbarComponent implements AfterViewInit {
+  @ViewChild('contentWrapper') content!: ElementRef;
+
+  ngAfterViewInit(): void {
+    console.log(this.content.nativeElement.children);
+    for (const child of this.content.nativeElement.children) {
+      child.draggable = false;
+      child.style.wordBreak = 'keep-all';
+      child.style.whitespace = 'no-wrap';
+    }
+
+    const slider = document.getElementById('overflow-container');
+    let isDown = false;
+    let startX = 0;
+    let scrollLeft = 0;
+
+    if (!slider) {
+      return;
+    }
+
+    slider.addEventListener('mousedown', (e: MouseEvent) => {
+      isDown = true;
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+    });
+    slider.addEventListener('mousemove', (e: MouseEvent) => {
+      if (!isDown) {
+        return;
+      }
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = x - startX; // * 2 scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  }
+}
