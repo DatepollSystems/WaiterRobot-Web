558a0b3be86a044b18fdd03b179d30635bef6a54
Dafnik <git@dafnik.me>
2022-11-28 15:54:58 +0100

    Remove old mobile-link url
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,6 @@ export const ROUTES: Routes = [
   {path: 'about', loadChildren: () => import('./about/about.routes').then((m) => m.ROUTES)},
   {path: 'home', loadChildren: () => import('./home/home.routes').then((m) => m.ROUTES)},
   {
-    path: 'mobile-link',
-    loadChildren: () => import('./mobile-link/mobile-link.component').then((m) => m.ROUTES),
-  },
-  {
     path: 'ml',
     loadChildren: () => import('./mobile-link/mobile-link.component').then((m) => m.ROUTES),
   },
