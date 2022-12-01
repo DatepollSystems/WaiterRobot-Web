b967d24cf9964d39928518ecefd6c5828abb5241
Dafnik <git@dafnik.me>
2022-12-01 16:04:35 +0100

    Fix auth interceptor
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/_shared/services/auth/auth-interceptor.ts
+++ src/app/_shared/services/auth/auth-interceptor.ts
@@ -1,7 +1,7 @@
 import {HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest} from '@angular/common/http';
 import {inject} from '@angular/core';
 
-import {Logger, loggerOf, WINDOW} from 'dfx-helper';
+import {loggerOf, WINDOW} from 'dfx-helper';
 import {BehaviorSubject, catchError, filter, Observable, switchMap, take, throwError} from 'rxjs';
 
 import {EnvironmentHelper} from '../../EnvironmentHelper';
@@ -42,7 +42,52 @@ export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn):
         if (error instanceof HttpErrorResponse) {
           switch (error.status) {
             case 401:
-              return handle401Error(authService, lumber, req, next);
+              /* When a session token was cancelled and the user decides to log out he gets an error and can't log out. This checks the url,
+               * clears the cookies and reloads the site. If the Angular app now checks isAuthenticated in the auth guard the
+               * app will route to sign in.
+               */
+              if (req.url.includes('/logout')) {
+                authService.clearStorage();
+
+                inject(WINDOW).location.reload();
+              }
+
+              if (!isRefreshing) {
+                isRefreshing = true;
+                refreshTokenSubject.next(undefined);
+
+                return authService.refreshJWTToken().pipe(
+                  switchMap((data: JWTResponse) => {
+                    lumber.info('handle401Error', 'JWT token refreshed');
+                    isRefreshing = false;
+                    refreshTokenSubject.next(data.token);
+                    return next(addToken(req, data.token));
+                  }),
+                  catchError(() => {
+                    lumber.error('handle401Error', 'Could not refresh jwt token with session token');
+                    if (EnvironmentHelper.getType() === 'prod') {
+                      authService.clearStorage();
+                      inject(WINDOW).location.reload();
+                    } else {
+                      inject(NotificationService).warning(
+                        'Something did not work out during the session refresh! Normally you would have been logged out and the window would have been force complete refreshed but for debugging purposes nothing happened!',
+                        25 * 1000
+                      );
+                    }
+
+                    return next(req);
+                  })
+                );
+              } else {
+                return refreshTokenSubject.pipe(
+                  filter((token) => token != undefined),
+                  take(1),
+                  switchMap((jwt: string) => {
+                    lumber.info('handle401Error', 'Already refreshing; JWT: "' + jwt + '"');
+                    return next(addToken(req, jwt));
+                  })
+                ) as Observable<HttpEvent<any>>;
+              }
             default:
               return throwError(error.error.message);
           }
@@ -56,7 +101,7 @@ export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn):
           return throwError(error.error.message);
         }
       })
-    ) as Observable<HttpEvent<unknown>>;
+    );
   }
 }
 
@@ -67,60 +112,3 @@ const addToken = (req: HttpRequest<any>, token?: string): HttpRequest<any> =>
       Authorization: `Bearer ${token ?? ''}`,
     },
   });
-
-const handle401Error = (
-  authService: AuthService,
-  lumber: Logger,
-  request: HttpRequest<any>,
-  next: HttpHandlerFn
-): Observable<HttpEvent<any>> => {
-  const notificationService = inject(NotificationService);
-  const window = inject(WINDOW);
-
-  /* When a session token was cancelled and the user decides to log out he gets an error and can't log out. This checks the url,
-   * clears the cookies and reloads the site. If the Angular app now checks isAuthenticated in the auth guard the
-   * app will route to sign in.
-   */
-  if (request.url.includes('/logout')) {
-    authService.clearStorage();
-
-    window.location.reload();
-  }
-
-  if (!isRefreshing) {
-    isRefreshing = true;
-    refreshTokenSubject.next(undefined);
-
-    return authService.refreshJWTToken().pipe(
-      switchMap((data: JWTResponse) => {
-        lumber.info('handle401Error', 'JWT token refreshed');
-        isRefreshing = false;
-        refreshTokenSubject.next(data.token);
-        return next(addToken(request, data.token));
-      }),
-      catchError(() => {
-        lumber.error('handle401Error', 'Could not refresh jwt token with session token');
-        if (EnvironmentHelper.getType() === 'prod') {
-          authService.clearStorage();
-          window.location.reload();
-        } else {
-          notificationService.warning(
-            'Something did not work out during the session refresh! Normally you would have been logged out and the window would have been force complete refreshed but for debugging purposes nothing happened!',
-            25 * 1000
-          );
-        }
-
-        return next(request);
-      })
-    );
-  } else {
-    return refreshTokenSubject.pipe(
-      filter((token) => token != undefined),
-      take(1),
-      switchMap((jwt: string) => {
-        lumber.info('handle401Error', 'Already refreshing; JWT: "' + jwt + '"');
-        return next(addToken(request, jwt));
-      })
-    ) as Observable<HttpEvent<any>>;
-  }
-};
