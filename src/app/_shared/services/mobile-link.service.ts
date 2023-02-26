b387d75f80a8d69b84531ca6608b073f84a524ea
Dafnik <git@dafnik.me>
2023-02-26 14:33:00 +0100

    update mobile link service to use string interpolation
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/_shared/services/mobile-link.service.ts
+++ src/app/_shared/services/mobile-link.service.ts
@@ -5,31 +5,29 @@ import {WINDOW} from 'dfx-helper';
   providedIn: 'root',
 })
 export class MobileLinkService {
-  constructor(@Inject(WINDOW) private window: Window) {}
+  constructor(@Inject(WINDOW) private window: Window | undefined) {}
 
   public createWaiterSignInLink(token: string): string {
-    return this.createWaiterAuthLink(token) + 'SIGN_IN';
+    return `${this.createWaiterAuthLink(token)}SIGN_IN`;
   }
 
   public createWaiterSignInViaCreateTokenLink(token: string): string {
-    return this.createWaiterAuthLink(token) + 'CREATE';
+    return `${this.createWaiterAuthLink(token)}CREATE`;
   }
 
   public createTableLink(tableId: string): string {
-    return this.createMobileLink() + 't/' + tableId;
+    return `${this.createMobileLink()}t/${tableId}`;
   }
 
   private createWaiterAuthLink(token: string): string {
-    return this.createMobileLink() + 'signIn' + '?token=' + token + '&purpose=';
+    return `${this.createMobileLink()}signIn?token=${token}&purpose=`;
   }
 
   private createMobileLink(): string {
-    return (
-      this.window.location.protocol +
-      '//' +
-      this.window.location.hostname +
-      (this.window.location.port.length > 0 ? ':' + this.window.location.port : '') +
-      '/ml/'
-    );
+    return this.window
+      ? `${this.window.location.protocol}//${this.window.location.hostname}${
+          this.window.location.port.length > 0 ? ':' + this.window.location.port : ''
+        }/ml/`
+      : '';
   }
 }
