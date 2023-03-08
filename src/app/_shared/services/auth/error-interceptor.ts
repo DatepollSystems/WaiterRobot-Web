d38b4b39206faec9b6ec3c45dc7ed6ea6806bf98
Dafnik <git@dafnik.me>
2023-03-08 22:37:20 +0100

    show errors not on auth refresh errors
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/_shared/services/auth/error-interceptor.ts
+++ src/app/_shared/services/auth/error-interceptor.ts
@@ -2,32 +2,16 @@ import {HttpEvent, HttpHandlerFn, HttpRequest} from '@angular/common/http';
 import {inject} from '@angular/core';
 import {catchError, Observable, throwError} from 'rxjs';
 import {NotificationService} from '../../../notifications/notification.service';
-import {AuthService} from './auth.service';
-
-/**
- * Don't intercept this requests
- */
-const paths = [AuthService.signInUrl, AuthService.signInPwChangeUrl, AuthService.refreshUrl];
 
 export function errorInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   const notificationService = inject(NotificationService);
 
-  let toIntercept = true;
-  for (const path of paths) {
-    if (req.url.includes(path)) {
-      toIntercept = false;
-      break;
-    }
-  }
-
-  if (!toIntercept) {
-    return next(req);
-  } else {
-    return next(req).pipe(
-      catchError((error) => {
+  return next(req).pipe(
+    catchError((error: {status: number}) => {
+      if (error?.status !== 401) {
         notificationService.terror('REQUEST_ERROR');
-        return throwError(() => error);
-      })
-    );
-  }
+      }
+      return throwError(() => error);
+    })
+  );
 }
