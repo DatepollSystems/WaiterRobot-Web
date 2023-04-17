55c1781d7c178a75df7bc1196ad8ddcbe0194d79
Dafnik <git@dafnik.me>
2023-04-17 13:51:36 +0200

    feat: remove unused services
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/_shared/services/abstract-model.service.ts
+++
@@ -1,9 +0,0 @@
-import {IEntityWithNumberID} from 'dfts-helper';
-import {AEntityService, ASelectableEntityService} from 'dfx-helper';
-
-export abstract class AbstractModelService<EntityType extends IEntityWithNumberID> extends AEntityService<number, EntityType> {}
-
-export abstract class AbstractSelectableModelService<EntityType extends IEntityWithNumberID> extends ASelectableEntityService<
-  number,
-  EntityType
-> {}
