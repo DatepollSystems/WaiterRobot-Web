37677a5a96c72ef43268515fb2e36f767b2d5d28
Dafnik <git@dafnik.me>
2023-06-17 23:49:03 +0200

    ci: add deploy

--- src/app/_shared/services/auth/user/my-user.service.ts
+++ src/app/_shared/services/auth/user/my-user.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {notNullAndUndefined, o_fromStorage, st_set} from 'dfts-helper';
-import {filter, map, merge, Observable, shareReplay, startWith, Subject, tap} from 'rxjs';
+import {o_fromStorage, st_set} from 'dfts-helper';
+import {map, merge, Observable, of, share, shareReplay, Subject, switchMap, tap} from 'rxjs';
 import {GetMyselfResponse} from '../../../waiterrobot-backend';
 
 import {MyUserModel} from './my-user.model';
@@ -16,13 +16,13 @@ export class MyUserService {
 
   public getUser$(): Observable<MyUserModel> {
     return merge(
-      this.httpClient.get<GetMyselfResponse>('/user/myself').pipe(
-        tap((it) => st_set('myuser_2', it)),
-        startWith(o_fromStorage<GetMyselfResponse>('myuser_2')),
-        filter(notNullAndUndefined),
+      of(o_fromStorage<GetMyselfResponse>('myuser_2')).pipe(
+        switchMap((it) =>
+          it ? of(it) : this.httpClient.get<GetMyselfResponse>('/user/myself').pipe(tap((iit) => st_set('myuser_2', iit)))
+        ),
         map((it) => new MyUserModel(it))
       ),
       this.manualUserChange.asObservable()
-    ).pipe(shareReplay(1));
+    ).pipe(share(), shareReplay(1));
   }
 }
