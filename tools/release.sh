1b580253e4e1bcadd9962351c9040785f284dcb2
Fabian Schedler <dev.fabian.schedler@gmail.com>
2024-01-28 15:33:29 +0100

    fix: release scripts

--- tools/release.sh
+++ tools/release.sh
@@ -3,4 +3,4 @@
 version=$(sed -nr 's/.*\"version\": \"([0-9]+\.[0-9]+\.[0-9]+)\".*/\1/p' package.json)
 
 git tag "$version"
-git tag push origin "$version"
+git push origin "$version"
--- tools/releaseLava.sh
+++ tools/releaseLava.sh
@@ -3,4 +3,4 @@
 version=$(sed -nr 's/.*\"version\": \"([0-9]+\.[0-9]+\.[0-9]+)\".*/\1/p' package.json)-lava-$(date +'%Y%m%d%H%M%S')
 
 git tag "$version"
-git tag push origin "$version"
+git push origin "$version"
