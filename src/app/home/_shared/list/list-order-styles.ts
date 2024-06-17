01ade230e9fbaf0f649b875852224130396348ae
Dafnik <git@dafnik.me>
2024-06-17 15:55:29 +0200

    fix: drag & drop style

--- src/app/home/_shared/list/list-order-styles.ts
+++ src/app/home/_shared/list/list-order-styles.ts
@@ -1,25 +1,30 @@
 export const listOrderStyles = `
     .cdk-drag-preview {
+      display: flex;
+      gap: 2rem;
       box-sizing: border-box;
       border-radius: 4px;
-      box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
-      0 8px 10px 1px rgba(0, 0, 0, 0.14),
-      0 3px 14px 2px rgba(0, 0, 0, 0.12);
-      display: table;
-      color: white;
+      color: var(--bs-body-color)
       background-color: var(--bs-body-bg);
       padding: 0 4px 0 4px;
+      position: relative;
+      z-index: 1;
+      transition: box-shadow 200ms cubic-bezier(0, 0, 0.2, 1);
+      box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+    }
+
+    tr {
+      background-color: var(--bs-body-bg);
     }
 
     .cdk-drag-preview td {
-      flex-grow: 2;
       vertical-align: middle;
       padding: 8px;
       cursor: grabbing;
     }
 
     .cdk-drag-placeholder {
-      background-color: var(--bs-tertiary-bg);
+      opacity: 0;
     }
 
     .cdk-drag-animating {
