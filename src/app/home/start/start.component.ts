3018489879d660913e8801e1d4bbc48945ac72ad
Dafnik <git@dafnik.me>
2024-08-12 09:48:43 +0200

    feat: add unconnected printers warning

--- src/app/home/start/start.component.ts
+++ src/app/home/start/start.component.ts
@@ -17,10 +17,14 @@ import {StopPropagationDirective} from 'dfx-helper';
 import {deriveLoading} from 'ngxtension/derive-loading';
 
 import {catchError, filter, map, of, startWith, switchMap, timer} from 'rxjs';
+import {catchError, combineLatest, filter, map, of, startWith, switchMap, timer} from 'rxjs';
+import {MyUserService} from '../_shared/services/user/my-user.service';
 import {SelectedEventService} from '../_admin/events/_services/selected-event.service';
 import {MyUserService} from '../_shared/services/user/my-user.service';
 import {AppOrderStateBadgeComponent} from '../orders/_components/app-order-state-badge.component';
 import {OrdersService} from '../orders/orders.service';
+import {MediatorsService} from '../printers/_services/mediators.service';
+import {PrintersService} from '../printers/_services/printers.service';
 
 @Component({
   selector: 'app-start',
@@ -43,6 +47,8 @@ export class StartComponent {
   #httpClient = inject(HttpClient);
   #authService = inject(AuthService);
   #ordersService = inject(OrdersService);
+  #mediatorsService = inject(MediatorsService);
+  #printersService = inject(PrintersService);
 
   isProduction = EnvironmentHelper.getProduction();
   type = EnvironmentHelper.getType();
@@ -76,6 +82,17 @@ export class StartComponent {
   orders = toSignal(this.#orders$);
 
   showOrdersLoading = toSignal(this.#orders$.pipe(deriveLoading({threshold: 0, loadingTime: 0})), {requireSync: true});
+
+  unusedPrinters = toSignal(
+    timer(0, 3 * 60000).pipe(
+      switchMap(() => combineLatest([this.#mediatorsService.getAll$(), this.#printersService.getAll$()])),
+      map(([mediators, printers]) => {
+        const allUsedPrinters = mediators.map((mediator) => mediator.printers.map((printer) => printer.id)).flat();
+        return printers.filter((printer) => !allUsedPrinters.includes(printer.id));
+      }),
+      map((printers) => (printers.length > 0 ? printers : undefined)),
+    ),
+  );
 
   logout(): void {
     this.#authService.logout();
