5e961b134ec37fee3a2820c3632961093079cbe7
Dafnik <git@dafnik.me>
2024-10-04 13:47:59 +0200

    feat: demo event alert and optimize loading of components on start and home layout
    Signed-off-by: Dafnik <git@dafnik.me>

--- src/app/home/_layout/components/active-system-notifications.component.ts
+++ src/app/home/_layout/components/active-system-notifications.component.ts
@@ -6,16 +6,18 @@ import {AppSystemNotificationAlertComponent} from './system-notification-alert.c
 
 @Component({
   template: `
-    @if (activeSystemNotificationsService.getFilteredSystemNotifications().length > 0) {
-      <div class="d-flex flex-column gap-3 my-2">
-        @for (systemNotification of activeSystemNotificationsService.getFilteredSystemNotifications(); track systemNotification.id) {
-          <app-system-notification-alert
-            [notification]="systemNotification"
-            [disableIgnore]="disableIgnore()"
-            (ignore)="activeSystemNotificationsService.ignore($event)"
-          />
-        }
-      </div>
+    @defer (when activeSystemNotificationsService.getFilteredSystemNotifications().length > 0) {
+      @if (activeSystemNotificationsService.getFilteredSystemNotifications().length > 0) {
+        <div class="d-flex flex-column gap-3 my-2">
+          @for (systemNotification of activeSystemNotificationsService.getFilteredSystemNotifications(); track systemNotification.id) {
+            <app-system-notification-alert
+              [notification]="systemNotification"
+              [disableIgnore]="disableIgnore()"
+              (ignore)="activeSystemNotificationsService.ignore($event)"
+            />
+          }
+        </div>
+      }
     }
   `,
   standalone: true,
@@ -31,31 +33,33 @@ export class ActiveSystemNotificationsComponent {
 
 @Component({
   template: `
-    @if (activeSystemNotificationsService.allSystemNotifications().length > 0) {
-      <div class="d-flex flex-column gap-3 my-2">
-        @if (activeSystemNotificationsService.getFilteredSystemNotifications().length > 0) {
-          <app-active-system-notifications />
-        }
-        <div class="d-flex justify-content-end">
-          @if (
-            activeSystemNotificationsService.ignoredSystemNotifications().length === 0 &&
-            activeSystemNotificationsService.getFilteredSystemNotifications().length > 1
-          ) {
-            <small>
-              <bi name="arrows-collapse" class="me-2" />
-              <a (mousedown)="activeSystemNotificationsService.ignoreAll()">{{ 'HIDE_ALL' | transloco }}</a>
-            </small>
+    @defer (when activeSystemNotificationsService.getFilteredSystemNotifications().length > 0) {
+      @if (activeSystemNotificationsService.allSystemNotifications().length > 0) {
+        <div class="d-flex flex-column gap-3 my-2">
+          @if (activeSystemNotificationsService.getFilteredSystemNotifications().length > 0) {
+            <app-active-system-notifications />
           }
-          @if (activeSystemNotificationsService.ignoredSystemNotifications().length > 0) {
-            <small>
-              <bi name="arrows-expand" class="me-2" />
-              <a (mousedown)="activeSystemNotificationsService.resetIgnore()">
-                {{ activeSystemNotificationsService.ignoredSystemNotifications().length }}x Nachricht(en) anzeigen
-              </a>
-            </small>
-          }
+          <div class="d-flex justify-content-end">
+            @if (
+              activeSystemNotificationsService.ignoredSystemNotifications().length === 0 &&
+              activeSystemNotificationsService.getFilteredSystemNotifications().length > 1
+            ) {
+              <small>
+                <bi name="arrows-collapse" class="me-2" />
+                <a (mousedown)="activeSystemNotificationsService.ignoreAll()">{{ 'HIDE_ALL' | transloco }}</a>
+              </small>
+            }
+            @if (activeSystemNotificationsService.ignoredSystemNotifications().length > 0) {
+              <small>
+                <bi name="arrows-expand" class="me-2" />
+                <a (mousedown)="activeSystemNotificationsService.resetIgnore()">
+                  {{ activeSystemNotificationsService.ignoredSystemNotifications().length }}x Nachricht(en) anzeigen
+                </a>
+              </small>
+            }
+          </div>
         </div>
-      </div>
+      }
     }
   `,
   standalone: true,
